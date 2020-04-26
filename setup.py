ceb93a50b7d231faaecd99cd6e8dc8a28a3755e9
Andrei Lapets <a@lapets.io>
2020-04-25 23:59:30 -0400

    Update package version.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.rst", "r") as fh:
 
 setup(
     name="bfcl",
-    version="0.1.0.0",
+    version="0.1.0.1",
     packages=["bfcl",],
     install_requires=["parts", "circuit",],
     license="MIT",
